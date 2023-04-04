d07165936c785b318510c8cc5f942a479155f0b5
nebraszka <majusxp@gmail.com>
2023-04-04 12:25:08 +0200

    Add prototypes of the essential parts of the CRUD project
    - emotion components and service
    - routing module
    - in memory data service

--- emotions-crud/src/app/in-memory-data.service.ts
+++ emotions-crud/src/app/in-memory-data.service.ts
@@ -8,8 +8,12 @@ import { Observable } from 'rxjs';
 export class InMemoryDataService implements InMemoryDbService{
   createDb() {
     const emotions = [
+<<<<<<< HEAD
       {id: 1, name: 'radość'},
       {id: 3, name: 'strach'}
+=======
+      {id: 1, name: 'radość'}
+>>>>>>> 9294fb0 (Add prototypes of the essential parts of the CRUD project)
     ];
     return {emotions};
   }
