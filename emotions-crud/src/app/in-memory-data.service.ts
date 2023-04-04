5fd39a7beddaee2be2f452e5989b0756d229d9b4
nebraszka <majusxp@gmail.com>
2023-04-04 13:15:24 +0200

    Emotions are now taken from the simulated server

--- emotions-crud/src/app/in-memory-data.service.ts
+++ emotions-crud/src/app/in-memory-data.service.ts
@@ -8,12 +8,9 @@ import { Observable } from 'rxjs';
 export class InMemoryDataService implements InMemoryDbService{
   createDb() {
     const emotions = [
-<<<<<<< HEAD
       {id: 1, name: 'radość'},
       {id: 3, name: 'strach'}
-=======
-      {id: 1, name: 'radość'}
->>>>>>> 9294fb0 (Add prototypes of the essential parts of the CRUD project)
+
     ];
     return {emotions};
   }
